e409914335d23887fae4014fd10e779eb8f80e7d
aljoker2001 <aljoker2001@hotmail.com>
2019-07-12 19:44:06 -0700

    added boilerplate files

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Wrapper from './components/Wrapper/index.js'
+
+function App() {
+  return (
+    <Wrapper>
+      <h1 className='jumbotron text-center'>Hello Friends!</h1>
+    </Wrapper>
+  );
+}
+
+export default App;
